12d83841bc854527311670cc973677597d71578a
xlnd47 <axel.deschuijmer@gmail.com>
2018-06-21 09:31:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NDU5MjU4MzU4MjA2MDM4MDY2.DgzlMA.vmHM-it0Pt50XDVBLfBNTFcKrAM);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
